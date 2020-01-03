73f5d57830d251d04ec05f6d7154e1bcf281927b
Keliang Liu <keliangliu1@gmail.com>
2020-01-03 17:43:56 +1100

    Add all buttons with id and value

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Grid, Typography, Button } from '@material-ui/core/';
 import './App.css';
 
+
+
+
+
+
 function App() {
+  // const [display, setDisplay] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Grid item id="display" xs={12}>
+        <Typography variant="h3" align='center' gutterBottom>
+          Hello World
+        </Typography>
+      </Grid>
+      {buttons.map(button => (
+        <Grid item>
+          <MyButton id={button.id} value={button.value} />
+        </Grid>
+      )
+      )}
+    </Grid>
   );
 }
 
 export default App;
+
+const buttons = [
+  { id: "zero", value: "0" },
+  { id: "one", value: "1" },
+  { id: "two", value: "2" },
+  { id: "three", value: "3" },
+  { id: "four", value: "4" },
+  { id: "five", value: "5" },
+  { id: "six", value: "6" },
+  { id: "seven", value: "7" },
+  { id: "eight", value: "8" },
+  { id: "nine", value: "9" },
+  { id: "add", value: "+" },
+  { id: "subtract", value: "-" },
+  { id: "multiply", value: "*" },
+  { id: "divide", value: "/" },
+  { id: "clear", value: "AC" },
+  { id: "decimal", value: "." },
+  { id: "equals", value: "=" }]
+
+const MyButton = (props) => {
+  return (
+    <Button
+      // style={{ height: 90 }}
+      variant='contained'
+      id={props.id}
+      value={props.value}
+      fullWidth>
+      {props.value}
+    </Button>
+  );
+}
